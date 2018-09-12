f4336093fffca88985a10911977970430ef274d5
evereux <evereux@gmail.com>
2018-09-12 19:12:22 +0100

    pytest circle

--- tests.py
+++ tests.py
@@ -1,11 +1,31 @@
-from application import ElipseCalc
 
-semimajor = 200
-semiminor = 80
-area = 15
-cfm = 8
+from math import pi
 
-EC = ElipseCalc(semimajor, semiminor, False, False)
+from application.circle import CircleCalc
 
-_, _, area, cfm, _ = EC.properties()
-print("Area: {}, CFM: {}".format(area, cfm))
+def test_circle():
+
+    # define circle defaults
+    diameter = 8
+    area = pi * ((diameter / 2) * (diameter / 2))
+    cfm = 2 * pi * (diameter / 2)
+
+    # calculate using diameter
+    circle = CircleCalc(diameter, 0, 0)
+    circle.properties()
+    assert circle.area == area
+    assert circle.cfm == cfm
+
+    # calculate using area
+    circle = CircleCalc(0, area, 0)
+    circle.properties()
+    assert circle.dia == diameter
+    assert circle.cfm == cfm
+
+    # calculate using cfm
+    circle = CircleCalc(0, 0, cfm)
+    circle.properties()
+    assert circle.dia == diameter
+    assert circle.area == area
+
+
